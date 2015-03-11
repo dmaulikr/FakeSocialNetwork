02efeb150798c6b85d94159d0d8cf4a4379b835c
Renato Camilio <renatomcamilio@gmail.com>
2015-03-11 15:50:58 -0700

    Initial Commit

---
+++ FakeSocialNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FakeSocialNetwork
+//
+//  Created by Renato Camilio on 2015-03-11.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FakeSocialNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FakeSocialNetwork
+//
+//  Created by Renato Camilio on 2015-03-11.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
