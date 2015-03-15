40378a2e95278643a2ab0ce7c39ea96dac635a8c
Renato Camilio <renatomcamilio@gmail.com>
2015-03-14 23:11:05 -0700

    Adapting TabBarHandler to use custom animator delegate

--- FakeSocialNetwork/FSNTabBarHandler.m
+++ FakeSocialNetwork/FSNTabBarHandler.m
@@ -7,6 +7,11 @@
 //
 
 #import "FSNTabBarHandler.h"
+#import "FSNTabBarAnimator.h"
+
+@interface FSNTabBarHandler ()
+@property (nonatomic, strong) FSNTabBarAnimator *animator;
+@end
 
 @implementation FSNTabBarHandler
 
@@ -16,48 +21,14 @@
     
     dispatch_once(&onceToken, ^{
         sharedInstance = [[self alloc] init];
+        sharedInstance.animator = [[FSNTabBarAnimator alloc] init];
     });
     
     return sharedInstance;
 }
 
-- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
-    // get the index to check later if we should animate right or left
-    int controllerIndex = [tabBarController.viewControllers indexOfObject:viewController];
-    if (controllerIndex == tabBarController.selectedIndex) {
-        return YES;
-    }
-    
-    // Get the views.
-    UIView *fromView = tabBarController.selectedViewController.view;
-    UIView *toView = [[tabBarController.viewControllers objectAtIndex:controllerIndex] view];
-    
-    // Get the size of the view area.
-    CGRect viewSize = fromView.frame;
-    BOOL shouldScrollRight = controllerIndex > tabBarController.selectedIndex;
-    
-    // Add the view to the tab bar view.
-    [fromView.superview addSubview:toView];
-    
-    // Position it off screen.
-    toView.frame = CGRectMake((shouldScrollRight ? 320 : -320), viewSize.origin.y, 320, viewSize.size.height);
-    
-    [UIView animateWithDuration:0.3
-                     animations: ^{
-                         // Animate the views on and off the screen. This will appear to slide.
-                         fromView.frame =CGRectMake((shouldScrollRight ? -320 : 320), viewSize.origin.y, 320, viewSize.size.height);
-                         toView.frame =CGRectMake(0, viewSize.origin.y, 320, viewSize.size.height);
-                     }
-     
-                     completion:^(BOOL finished) {
-                         if (finished) {
-                             // Remove the old view from the tabbar view.
-                             [fromView removeFromSuperview];
-                             tabBarController.selectedIndex = controllerIndex;                
-                         }
-                     }];
-    
-    return YES;
+- (id<UIViewControllerAnimatedTransitioning>)tabBarController:(UITabBarController *)tabBarController animationControllerForTransitionFromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC {
+    return self.animator;
 }
 
 @end
